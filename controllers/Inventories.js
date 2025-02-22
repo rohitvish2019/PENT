3629a01917540ce2ce12901d7ea71e1399c45951
Rohit Vishwakarma <rohitbca2019@gmail.com>
2025-02-22 12:44:24 +0530

    backend changes

--- controllers/Inventories.js
+++ controllers/Inventories.js
@@ -26,3 +26,45 @@ module.exports.getItemByName = async function(req, res) {
     }
 }
 
+module.exports.getAllItems = async function (req, res) {
+    try {
+        let items = await InventoriesData.aggregate([
+            {
+                $group: {
+                    _id: "$Name",  // Group by name
+                    TotalQuantity: { $sum: "$Quantity" },  // Sum the quantities
+                    Category: { $first: "$Category" }, // Pick first category (assuming consistent category for same name)
+                    Batch: { $addToSet: "$Batch" }, // Pick first batch (modify if needed)
+                    ExpiryDate: { $first: "$ExpiryDate" }, // Pick first expiry date
+                    Price: { $first: "$Price" }, // Pick first price
+                    Quantity : { $min : 0},
+                    AlertQty : { $avg : "$AlertQty"}
+
+                }
+            },
+            {
+                $match: { TotalQuantity: { $gt: 0 } }  // Filter where quantity > 0
+            },
+            {
+                $project: {
+                    _id: 0,
+                    Name: "$_id",
+                    TotalQuantity: 1,
+                    Category: 1,
+                    Batch: 1,
+                    ExpiryDate: 1,
+                    Price: 1,
+                    AlertQty:1
+                }
+            }
+        ]);
+        return res.status(200).json({
+            items
+        })
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({
+            message :'Error fetching inventories'
+        })
+    }
+}
